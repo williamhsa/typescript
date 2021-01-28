17250ca545f834efdfbf02b65c1bc0045500a769
williamhsa <whsa2021>
2021-01-27 23:14:07 -0300

    update

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
 console.log('Your code goes here!')
+
+function add (n1: number, n2: number): number {
+  return n1 + n2
+}
+
+
+function addSecond (n1: number, n2: number): number {
+  if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    throw new Error('Incorrect input!')
+  }
+  return n1 + n2
+}
+
+const number1 = 5
+const number2 = 2.8
+
+const result = add(number1, number2)
+
+console.log('result: ', result)
