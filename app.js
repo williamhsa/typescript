ea70302f184097f82073bdbc27e82d8710dbb8cf
williamhsa <whsa2021>
2021-02-22 22:12:04 -0300

    type assignment & type inference

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ console.log('Your code goes here!');
 function add(n1, n2) {
     return n1 + n2;
 }
+function addSecond(n1, n2, message) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorrect input!');
+    }
+    if (typeof message === 'string')
+        console.log('oi');
+    return n1 + n2;
+}
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var result = addSecond(number1, number2, 'max');
 console.log('result: ', result);
--- app.ts
+++ app.ts
@@ -5,16 +5,19 @@ function add (n1: number, n2: number): number {
 }
 
 
-function addSecond (n1: number, n2: number): number {
+function addSecond (n1: number, n2: number, message: string): number {
   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     throw new Error('Incorrect input!')
   }
+
+  if (typeof message === 'string') console.log('oi')
+
   return n1 + n2
 }
 
 const number1 = 5
 const number2 = 2.8
 
-const result = add(number1, number2)
+const result = addSecond(number1, number2, 'max')
 
 console.log('result: ', result)
