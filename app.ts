351d4f8a0c78e59799ea97611a7ba26a3a7c0210
williamhsa <whsa2021>
2021-03-08 18:28:27 -0300

    functions return Types & 'void'

---
+++ app.ts
@@ -0,0 +1,17 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log('Result ' + num);
+  return;
+}
+
+printResult(add(5,5));
+
+
+
+// function printResultTwo(num: number): undefined {
+//   console.log('Result ' + num);
+//   return
+// }
